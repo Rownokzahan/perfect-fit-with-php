49f35acb6f622764d548cf2f554ce39a54aa5ab0
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2022-11-28 22:55:03 +0600

    admin panal

--- script/home-page.js
+++ script/home-page.js
@@ -1,3 +1,5 @@
+//--------For Customer Home Page---------
+
 $('#home-icon').click(function(){
     $('#home-info').css("display","block");
 
@@ -17,3 +19,36 @@ $('#phone-icon').click(function(){
     $('#mail-info').css("display","none");
 });
 
+//--------For Admin Home Page---------
+
+$('#dress-info').click(function(){
+    if($('#dress-info-detail').html() !=""){
+        $('#dress-info-detail').html('');
+    }
+    else{
+        $('#dress-info-detail').html(`
+            <a href="category.html" class="nav-link">                            
+                <i class="bi bi-stack"></i>
+                Category
+            </a>
+            <a href="product.html" class="nav-link">
+                <i class="bi bi-images"></i>    
+                Dresses
+            </a>
+        `); 
+    }
+});
+
+$('#customization').click(function(){
+    if($('#customization-detail').html() !=""){
+        $('#customization-detail').html('');
+    }
+    else{
+        $('#customization-detail').html(`
+            <a href="" class="nav-link">
+                <i class="bi bi-rulers"></i>
+                Measurement
+            </a>
+        `); 
+    }
+});
